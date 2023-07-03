28f28aa6fd75e9e58e3751eb7e711d877693ba89
shkiimaa <shkiimaa@gmail.com>
2023-07-03 17:47:34 +0900

    layout 컴포넌트, Link 태그

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <h1>홈페이지</h1>;
+  return <h1>홈페이지 경로</h1>;
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+
+const products = ['shirt', 'pants', 'skirt', 'shose'];
+
 export default function ProductsPage() {
-  return <h1>제품 소개 페이지</h1>;
+  return (
+    <>
+      <h1>제품 소개 페이지</h1>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product}>
+              <Link href={`products/${product}`}>{product}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
