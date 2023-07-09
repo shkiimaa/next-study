23fe9a54f61cf61755b2b3772ed4dd5ed1d276c9
shkiimaa <shkiimaa@gmail.com>
2023-07-09 20:48:49 +0900

    redirects 추가

--- next.config.js
+++ next.config.js
@@ -10,6 +10,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever',
+        destination: '/products',
+        permanent: true, // 308코드를 보내주어 영원이 이동했다고 서치엔진에 알려주는 역할
+      },
+      {
+        source: '/products/deleted_temp',
+        destination: '/products',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
