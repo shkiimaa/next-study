29fcb1258e1ab9ea76b6e22314627015a857ee69
shkiimaa <shkiimaa@gmail.com>
2023-07-09 23:07:07 +0900

    api 라우트 수정

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -5,3 +5,20 @@ export async function GET(req: Request) {
   const products = await getProducts();
   return NextResponse.json({ products });
 }
+
+//12버전 api 라우팅
+// {
+//   type Data = {
+//     name: string;
+//   };
+
+//   export async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse<Product[]>
+//   ) {
+//     if (req.method === 'GET') {
+//       const products = await getProducts();
+//       return res.status(200).json(products);
+//     }
+//   }
+// }
